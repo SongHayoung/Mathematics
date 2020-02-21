685aa5a5e824b425c3018a8942018c24a2f006f5
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2020-02-21 22:38:38 +0900

    확장 유클리드 호제법 A*x + B*y = gcd(A,B)

--- ECC.c
+++ ECC.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+struct Solution { int gcd, x, y; };
 int s[10000], t[10000], r[10000], q[10000];
 int i;
 void INIT_EEC_Table(int A, int B){
@@ -24,12 +25,20 @@ void EEC(int A,int B){
     }
     Show_Table(); 
 }
+Solution extendedEuclid(int a, int b) {
+	int q = a / b, r = a % b;
+	if (r == 0) return Solution{ b, 0, 1 };
+	Solution s = extendedEuclid(b, r);
+	return Solution{ s.gcd, s.y, s.x - q * s.y };
+}
 int main(int argc, char** argv)
 {
     int A, B;
     scanf("%d %d",&A,&B);
     EEC(A,B);
     printf("%d %d\n",s[i],t[i]);
+    Solution s = extendedEuclid(A,B);
+    printf("%d %d\n",s.x, s.y);
     return 0; //정상종료시 반드시 0을 리턴해야 합니다.
 }
 /*
